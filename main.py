56836d7c86a17473f602acc8312f923b68491939
António Fernandes <antonio.mf97@gmail.com>
2022-07-07 21:34:20 +0100

    UPD: updated main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 """
 Project: EGT-Sim
+
+Filename: main.py
+Description: running and testing
 """
 
 
+def run():
+    """Project runner for given input"""
+    pass
+
+
 if __name__ == '__main__':
-    print("This is a main file")
+    run()
