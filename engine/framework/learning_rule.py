ad3524116ff75ae9fd2a7d4a62e8a8d3930385a6
António Fernandes <antonio.mf97@gmail.com>
2022-07-25 23:41:22 +0100

    UPD: updated learning rules

---
+++ engine/framework/learning_rule.py
@@ -0,0 +1,20 @@
+
+class LearningRule:
+    def __init__(self):
+        pass
+
+
+class Conformity(LearningRule):
+    def __init__(self):
+        super().__init__()
+
+
+class SocialLearning(LearningRule):
+    def __init__(self):
+        super().__init__()
+
+
+class CounterfactualThinking(LearningRule):
+    def __init__(self):
+        super().__init__()
+
