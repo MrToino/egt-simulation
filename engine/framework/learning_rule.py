8f7377609a16400d86a6cb11b01d4635a6617b84
António Fernandes <antonio.mf97@gmail.com>
2022-07-28 00:01:28 +0100

    UPD: updated learning rules

--- engine/framework/learning_rule.py
+++ engine/framework/learning_rule.py
@@ -2,23 +2,32 @@ from game import Game
 
 
 class LearningRule:
-    def __init__(self):
-        pass
-
+    def __init__(self, configs):
+        self.size = configs["size"]
+        self.intensity = configs["intensity"]
 
 class Conformity(LearningRule):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, configs):
+        super().__init__(configs)
+
+    def fractionC(self, k):
+        return k / self.size
+
+    def fractionD(self, k):
+        return (self.size - k) / self.size
+
+    def Fermi(self, k):
+        return - self.intensity * (self.fractionD(k) - self.fractionC(k))
 
 
 class SocialLearning(LearningRule):
     def __init__(self, configs):
-        super().__init__()
+        super().__init__(configs)
         self.game = Game(configs)
 
 
 class CounterfactualThinking(LearningRule):
     def __init__(self, configs):
-        super().__init__()
+        super().__init__(configs)
         self.game = Game(configs)
 
