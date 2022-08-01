79779e16c3efcb2ab0e59df9a22fc8a0bd6e5833
António Fernandes <antonio.mf97@gmail.com>
2022-08-01 14:25:39 +0100

    UPD: updated framework and population

--- engine/framework/framework.py
+++ engine/framework/framework.py
@@ -4,7 +4,7 @@ from population import Population
 class Framework:
     def __init__(self, pop_configs):
         self.pop_configs = pop_configs
-        self.framework_type = pop_configs["FrameworkType"]
+        self.framework_type = self.pop_configs["FrameworkType"]
         self.populations = self.build_populations()
 
     def build_populations(self):
---
+++ engine/framework/population.py
@@ -0,0 +1,3 @@
+class Population:
+    def __init__(self, population_configs):
+        self.configs = population_configs
