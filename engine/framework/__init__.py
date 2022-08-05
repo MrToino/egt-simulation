63a89fbe2523e0579df6685c84d97a19b53dd96e
António Fernandes <antonio.mf97@gmail.com>
2022-08-05 23:54:12 +0100

    UPD: updated framework package

---
+++ engine/framework/__init__.py
@@ -0,0 +1,6 @@
+from framework import Framework
+from game import Game2P
+from game import NSnowdriftGame
+from game import NStagHunt
+from game import PublicGoodGames
+from game import CollectiveRiskDilemma
