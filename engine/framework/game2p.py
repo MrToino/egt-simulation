2ea0d36d99c579815443a42e32f2be9edcf19f65
António Fernandes <antonio.mf97@gmail.com>
2022-08-10 22:26:52 +0100

    UPD: updated 2p games (complete)

--- engine/framework/game2p.py
+++ engine/framework/game2p.py
@@ -6,3 +6,20 @@ Package: framework
 Filename: game2p.py
 Description: defines the 2-person game framework
 """
+
+from engine.framework.game import Game
+
+
+class Game2P(Game):
+    def __init__(self, game_configurations):
+        """Initialize the general game class"""
+        super().__init__(game_configurations)
+        self.R, self.S, self.T, self.P = self.configs["R", "S", "T", "P"]
+
+    def fitnessC(self, i):
+        """Returns the fitness for cooperators"""
+        return ((i - 1) * self.R + (self.Z - i) * self.S) / (self.Z - 1)
+
+    def fitnessD(self, i):
+        """Returns the fitness for defectors"""
+        return (i * self.T + (self.Z - i - 1) * self.P) / (self.Z - 1)
