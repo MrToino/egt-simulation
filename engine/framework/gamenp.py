51088c45ec0cc6d7c0bd2558a6bde9034dde9caf
António Fernandes <antonio.mf97@gmail.com>
2022-08-13 18:08:02 +0100

    UPD: updated nperson stag hunt game definition

--- engine/framework/gamenp.py
+++ engine/framework/gamenp.py
@@ -21,21 +21,21 @@ class GameNP(Game):
         self.N = self.configs["N"]
 
     def payoffC(self, i: int) -> float:
-        """Returns the payoff for cooperators"""
+        """Return the payoff for cooperators"""
         pass
 
     def payoffD(self, i: int) -> float:
-        """Returns the payoff for defectors"""
+        """Return the payoff for defectors"""
         pass
 
     def fitnessC(self, i: int) -> float:
-        """Returns the fitness for cooperators"""
+        """Return the fitness for cooperators"""
         return 1 / binom(self.Z - 1, self.N - 1) * sum(
             [binom(i - 1, j) * binom(self.Z - i, self.N - j - 1) * self.payoffC(j + 1)
              for j in range(0, self.N, 1)])
 
     def fitnessD(self, i: int) -> float:
-        """Returns the fitness for defectors"""
+        """Return the fitness for defectors"""
         return 1 / binom(self.Z - 1, self.N - 1) * sum(
             [binom(i, j) * binom(self.Z - i - 1, self.N - j - 1) * self.payoffD(j)
              for j in range(0, self.N, 1)])
@@ -50,11 +50,11 @@ class NSnowdriftGame(GameNP):
         self.M, self.b, self.c = self.configs["M", "b", "c"]
 
     def payoffC(self, i) -> float:
-        """Computes the payoff for cooperators"""
+        """Compute the payoff for cooperators"""
         return (-self.c / self.M if i < self.M else self.b - self.c / i) if i > 0 else 0
 
     def payoffD(self, i) -> float:
-        """Computes the payoff for defectors"""
+        """Compute the payoff for defectors"""
         return 0 if i < self.M else self.b
 
 
@@ -64,15 +64,15 @@ class NStagHunt(GameNP):
     def __init__(self, game_specs):
         """Initialize the general game class"""
         super().__init__(game_specs)
-        self.M, self.b, self.c = self.configs["M", "b", "c"]
+        self.M, self.F, self.c = self.configs["M", "F", "c"]
 
     def payoffC(self, i) -> float:
-        """Computes the payoff for cooperators"""
-        return (-self.c / self.M if i < self.M else self.b - self.c / i) if i > 0 else 0
+        """Compute the payoff for cooperators"""
+        return (-self.c if 1 < i < self.M else i * self.c * self.F / self.N - self.c) if i > 0 else 0
 
     def payoffD(self, i) -> float:
-        """Computes the payoff for defectors"""
-        return 0 if i < self.M else self.b
+        """Compute the payoff for defectors"""
+        return 0 if i < self.M else i * self.c * self.F / self.N
 
 
 class PublicGoodGames(GameNP):
